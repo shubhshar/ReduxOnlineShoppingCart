ff481b272bba21b8b02936443021c6f5d9d12456
Shubham Sharma <shubhr0813@gmail.com>
2023-02-08 02:12:51 +0530

    update in Cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,29 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { remove } from "../store/cartSlice";
+
 const Cart = () => {
+  const products = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  const handleRemove = (productId) => {
+    dispatch(remove(productId));
+  };
+
   return (
     <>
-      <div className="c">
-        <h1>Cart Component is rendering here</h1>
+      <div className="cartWrapper">
+        <h3>Cart</h3>
+        {products.map((product) => (
+          <div key={product.id} className="cartCard">
+            <img src={product.image} alt="prodimage" />
+            <h4>{product.title}</h4>
+            <h5>INR {product.price}</h5>
+            <button onClick={() => handleRemove(product.id)} className="btn">
+              Remove
+            </button>
+          </div>
+        ))}
       </div>
     </>
   );
