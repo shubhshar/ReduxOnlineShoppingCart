23b2636166ee6593f62fbf141738bd74036347e1
Shubham Sharma <shubhr0813@gmail.com>
2023-02-11 21:54:17 +0530

    implement loading and completed redux shopping cart

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,40 +1,49 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { add } from "../store/cartSlice";
+import { fetchProducts } from "../store/productSlice";
+import { STATUSES } from "../store/productSlice";
 
 const Products = () => {
   const dispatch = useDispatch();
-  const [products, setProducts] = useState([]);
-  const fetchProducts = async () => {
-    const res = await fetch("https://fakestoreapi.com/products"); //Api integration here
-    const data = await res.json();
-    console.log(data);
-    setProducts(data);
-  };
+  const { data: products, status } = useSelector((state) => state.product);
+  // const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetchProducts();
+    dispatch(fetchProducts());
+    // const fetchProducts = async () => {
+    //     const res = await fetch('https://fakestoreapi.com/products');
+    //     const data = await res.json();
+    //     console.log(data);
+    //     setProducts(data);
+    // };
+    // fetchProducts();
   }, []);
 
   const handleAdd = (product) => {
     dispatch(add(product));
   };
 
+  if (status === STATUSES.LOADING) {
+    return <h2>Loading....</h2>;
+  }
+
+  if (status === STATUSES.ERROR) {
+    return <h2>Something went wrong!</h2>;
+  }
   return (
-    <>
-      <div className="productsWrapper">
-        {products.map((product) => (
-          <div className="card" key={product.id}>
-            <img src={product.image} alt="prodimage" />
-            <h4>{product.title}</h4>
-            <h5>INR {product.price}</h5>
-            <button onClick={() => handleAdd(product)} className="btn">
-              Add to cart
-            </button>
-          </div>
-        ))}
-      </div>
-    </>
+    <div className="productsWrapper">
+      {products.map((product) => (
+        <div className="card" key={product.id}>
+          <img src={product.image} alt="" />
+          <h4>{product.title}</h4>
+          <h5>{product.price}</h5>
+          <button onClick={() => handleAdd(product)} className="btn">
+            Add to cart
+          </button>
+        </div>
+      ))}
+    </div>
   );
 };
 
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSliceReducer from "./cartSlice";
+import productReducer from "./productSlice";
 
 //We can say that it acts as combineReducer
 const store = configureStore({
   reducer: {
+    product: productReducer,
     cart: cartSliceReducer
   }
 });
